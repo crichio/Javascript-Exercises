91a3baab4fd0e9854460e4b4584651cf7b370396
crichio <christianricciardi@outlook.com>
2025-05-10 19:50:09 -0700

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,9 @@
+// exercise in declaring variables of different data types and printing them to the screen
+
+let myName = "Christian";     // variable that holds my name
+let age = 26;                 // variable that holds my age
+let male = true;              // variable that holds a boolean value on whether I am male or not
+
+console.log(myName, age, male); // prints all above variables to the screen
+ 
+
