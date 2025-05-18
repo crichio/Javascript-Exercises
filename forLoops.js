4e28c0ec40c373a7ca957c9b55a8f9ab2dc010c1
crichio <christianricciardi@outlook.com>
2025-05-17 21:26:05 -0700

    Add files via upload

---
+++ forLoops.js
@@ -0,0 +1,6 @@
+// simple for loop that counts to 10
+
+for (let i = 1; i <= 10; i++) {     // set variable i to 1, if i is less than 10, then increase i by 1
+    console.log(i);                 // prints out 1 until is is equal to 10
+}
+
