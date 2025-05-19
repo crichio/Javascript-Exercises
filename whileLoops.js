a8891bb043d54ed093e2ed9913482c832442716e
crichio <christianricciardi@outlook.com>
2025-05-18 17:25:52 -0700

    Add files via upload

---
+++ whileLoops.js
@@ -0,0 +1,9 @@
+// exercise in while loops
+
+// code prints numbers 1 - 5 on to the console
+
+let i = 1;                // declares variable i and assigns it the number 1
+while (i < 6) {           // as long as i is less than 6...
+    console.log(i);       // print i
+    i++                   // after that, increase i by 1
+}
