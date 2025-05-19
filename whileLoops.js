0c0830bb459fb714016a2c141b2204ab03c1d05b
crichio <christianricciardi@outlook.com>
2025-05-18 17:23:45 -0700

    Delete whileLoops.js

--- whileLoops.js
+++
@@ -1,7 +0,0 @@
-// exercise in while loops
-
-let i = 1;                // declares variable i and assigns it the number 1
-while (i < 6) {          // as long as i is less than 6...
-    console.log(i);      // print i
-    i++                  // after that, increase i by 1
-}
