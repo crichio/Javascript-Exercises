d9cf4cfea36ed418d37b7805ec4d6226dc19b461
crichio <christianricciardi@outlook.com>
2025-05-23 16:04:11 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+// functions !
+
+function add(a, b) {            // declares a function, names it add, and adds 2 parameters
+    return a + b;               // body of the function that returns parameters a plus b
+}
+
+let answer = add(123, 456);     // declares a variable and assigns it to function add
+
+console.log(`The answer is ${answer}`);     // prints "The answer is 579" to the screen by calling variable answer that then runs the function add to get 579
+
